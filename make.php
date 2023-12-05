be1518a3fc84fe292ec88a3101d01f3db8d874cc
Bultut-yegon <bultut9785@gmail.com>
2023-12-05 22:15:32 +0300

    php

---
+++ make.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <title>Intro to PHP</title>
+    </head>
+    <body>
+        <h1>My 1st php page</h1>
+        <?php echo "Hello world!";
+        $txt="hey";
+        ?>
+    </body>
+</html>
